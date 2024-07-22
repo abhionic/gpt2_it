a352246ead346299300ef315bcb10c0958b78090
Abhishek <48456430+abhionic@users.noreply.github.com>
2024-07-22 19:18:13 -0400

    inception

---
+++ infer.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import os; import time
+
+st.title('Abhi Micro Instruct LM')
+os.environ['KAGGLE_USERNAME'] = st.secrets['kaggle_username']
+os.environ['KAGGLE_KEY'] = st.secrets['kaggle_key']
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+# os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.9'
+import keras_nlp
+
+# initialize chat history
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+# display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+# load the model once and use it across all users and sessions
+@st.cache_resource
+def load_model():
+    return keras_nlp.models.CausalLM.from_preset(f'kaggle://abhionic/gpt2/keras/gpt2_it')
+
+# gpt2_base_en, gpt2_medium_en, trained on webtext, with 12 and 24 layers
+gpt2_it = load_model()
+template = 'Instruction:\n{instruction}\n\nResponse:\n{response}'
+
+# react to user input
+if prompt := st.chat_input('what is up (experimental)?'):
+    # add user message to chat history
+    st.session_state.messages.append({'role': 'user', 'content': prompt})
+    # display user message in chat message container
+    with st.chat_message('user'):
+        st.markdown(prompt)
+    context = template.format(instruction=prompt, response='',)
+    answer = gpt2_lm.generate(prompt, max_length=256)
+    def stream_data():
+        for word in answer.split(' '):
+            yield word + ' '
+            time.sleep(0.02)
+
+    # display assistant response in chat message container
+    with st.chat_message('assistant'):
+        response = st.write_stream(stream_data)
+    # add assistant response to chat history
+    st.session_state.messages.append({'role': 'assistant', 'content': response})
